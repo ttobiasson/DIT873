7be2ea83da380b95e377c6bf8793131826b5beb9
tomt <tomt@student.chalmers.se>
2020-03-25 19:06:53 +0100

    ändringar

--- Labbar/Lab1/assignment1.py
+++ Labbar/Lab1/assignment1.py
@@ -1,8 +1,24 @@
 import matplotlib.pyplot as plotter 
 import montecarlo
+import timeit
+import argparse
+#'time', '/home/tom/DIT873/Labbar/Lab1/montecarlo.py', '-w', '1', '-s', '1000000'
+parser = argparse.ArgumentParser(description='Compute Pi using Monte Carlo simulation.')
+parser.add_argument('--workers', '-w',
+                        default='1',
+                        type = int,
+                        help='Number of parallel processes')
+parser.add_argument('--steps', '-s',
+                        default='1000',
+                        type = int,
+                        help='Number of steps in the Monte Carlo simulation')
+args = parser.parse_args()
+tidendettog = timeit.timeit(str(montecarlo.compute_pi(args)))
+print(tidendettog)
 
-x1 = [1, 2, 4, 8, 16, 32] 
-y1 = [x for x in x1] 
+y1 = [1, 2, 4, 8, 16, 32]
+x1 = [1/x for x in y1]
+
 plotter.plot(x1, y1, label = "theoretical") 
   #
 x2 = [12,42,3] 
@@ -13,8 +29,5 @@ plotter.xlabel('Time in S')
 plotter.ylabel('Number of cores') 
 plotter.title('Parallelization graph') 
   
-# show a legend on the plot 
 plotter.legend() 
-  
-# function to show the plot 
 plotter.show() 
