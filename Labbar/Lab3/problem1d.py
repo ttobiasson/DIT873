c26592b36f594e74a069b65fd68cfbfc155b263a
tomt <tomt@student.chalmers.se>
2020-04-27 17:58:53 +0200

    problem1d

---
+++ Labbar/Lab3/problem1d.py
@@ -0,0 +1,37 @@
+from mrjob.job import MRJob
+import re
+import numpy as np
+
+NUMBERS_RE = re.compile(r'[0-9]\t[1-9]+\.[0-9]+$')
+
+class MRGoodJob(MRJob):
+
+    def mapper_init(self):
+        self.group = self.options.group
+
+    def configure_args(self):
+        super(MRGoodJob, self).configure_args()
+        self.add_passthru_arg('--group', default=2,type=int)
+
+    def mapper(self, _, line):
+        list = re.split(r'\t', line)
+        if int(list[1]) == self.group:
+            yield 1, float(list[2])
+
+    def reducer(self, key, numbers):
+        ns = [number for number in numbers]
+        
+        minimum = min(ns)
+        maximum = max(ns)
+        sd = np.std(ns)
+        mean = np.mean(ns)
+        histogram, edges = np.histogram(ns)
+        yield "Standard Deviation", sd
+        yield "Mean", mean
+        yield "Minimum", minimum
+        yield "Maximum", maximum
+        yield "Edges", edges.tolist()
+        yield "Histogram", histogram.tolist()
+
+if __name__ == '__main__':
+    MRGoodJob.run()
