6a484e5667fe50e0b1162d677786a32b44d2e194
tomt <tomt@student.chalmers.se>
2020-04-27 14:31:32 +0200

    ändringar

--- Pythonuppgifter/Python 3/Solution.py
+++ Pythonuppgifter/Python 3/Solution.py
@@ -3,14 +3,13 @@
 def sublist(s_list):
     list = []
     lista = []
-    len_s_list = len(s_list)
 
-    if len_s_list <= 1:
+    if len(s_list) <= 1:
         yield [s_list]
     if len((dict.fromkeys(s_list))) == 1:
         yield [[s_list[0]]]
 
-    for i in range(1, len_s_list):
+    for i in range(1, len(s_list)):
         list.append(s_list[i-1])
         lista = lista[:] + [[elem for elem in list]]
         
@@ -23,16 +22,12 @@ def longest_common_list(s_list):
     
     list = [ elem for elem in next(sublist(s_list)) ]
     revlist = [ elem for elem in next(sublist(s_list[::-1])) ]
-    #print(list)
-    #print(revlist)
+
     resultlist = [elem for elem in list for elem in revlist if elem in list]
-    #print(resultlist)
+
     return max(resultlist, key = len)
     
 
-
-# The following is called if you execute the script from the commandline
-# e.g. with python Solution.py
 if __name__ == "__main__":
     assert longest_common_list([1,1,2,3,0,0,3,4,5,7,1,3,2,1,1,2]) == [1,1, 2, 3]
     assert longest_common_list([2,1,1,2,3,1,7,5,4,3,0,0,3,2,1,1]) == [1,1, 2, 3]
